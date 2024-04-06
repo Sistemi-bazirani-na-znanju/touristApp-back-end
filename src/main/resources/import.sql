9227f781bbbbb184515335a2def38d5fb4473b3c
kirdatatjana <kirda.tatjana@gmail.com>
2024-04-06 15:32:44 +0100

    fixed bugs

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,15 +3,15 @@ INSERT INTO demo_model(name) VALUES ('Prvi');
 INSERT INTO demo_model(name) VALUES ('Drugi');
 
 -- Insert Arrangement data
-INSERT INTO arrangement (name, type, price, average_rating) VALUES ('Arrangement 1', 0, 100.0, 4.5);
+INSERT INTO arrangement (name, type, price, average_rating) VALUES ('Arrangement 1', 1, 100.0, 4.5);
 INSERT INTO arrangement (name, type, price, average_rating) VALUES ('Arrangement 2', 0, 150.0, 4.0);
 INSERT INTO arrangement (name, type, price, average_rating) VALUES ('Arrangement 3', 0, 200.0, 4.2);
 
 -- Insert Excursion data
 -- Please note that you need to specify arrangement_id based on your existing Arrangement data
-INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (1, 'Excursion 1', 50.0, 20, 0, 1000.0);
-INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (1, 'Excursion 2', 60.0, 25, 1, 1200.0);
-INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (2, 'Excursion 3', 70.0, 30, 2, 1400.0);
+INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (1, 'Excursion 1', 500.0, 20, 0, 1000.0);
+INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (1, 'Excursion 2', 6000.0, 25, 1, 1200.0);
+INSERT INTO excursion (arrangement_id, name, price, number_of_people_registered, type, total_price) VALUES (2, 'Excursion 3', 2000.0, 30, 2, 1400.0);
 
 -- Insert Rating data
 -- Please note that you need to specify arrangement_id based on your existing Arrangement data
